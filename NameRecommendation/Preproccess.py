28292ee29451d01b2d871af10ac39672576a1895
Mohammad Ramezani <mohammad0ramezani@outlook.com>
2022-01-11 00:18:01 +0330

    preproccess

--- NameRecommendation/Preproccess.py
+++ NameRecommendation/Preproccess.py
@@ -1,5 +1,6 @@
-        
-def file_pre_process(self,metric_file):   
+
+
+def pre_process(self,metric_file):   
     csv_file = pd.read_csv(metric_file, index_col=None, header=None, low_memory=False)
     csv_file = csv_file.iloc[1:, :]
     csv_file = csv_file.iloc[:, :].replace([None], np.nan)
@@ -8,3 +9,6 @@ def file_pre_process(self,metric_file):
     csv_file = csv_file[re[3].notna()]
     csv_file.iloc[:, 4:] = Normalizer().fit_transform(csv_file.iloc[:, 4:].replace(np.nan, 0))
     return normalized_files.append(csv_file)
+
+def split_files(self,inputfile):
+    return [inputfile.iloc[:, 4:], inputfile.iloc[:, :4]]
--- Utilization/constants.py
+++ Utilization/constants.py
@@ -1,10 +1,8 @@
 
-
-class Constants:
-        def __init__(self):
-            self.MethodsDatapkl='/Data/MethodsData.pkl'
-            self.MethodsMetricsDir='/Data/MethodsMetrics'
-            self.nneighbors=10
-            self.algorithm='auto'
-            self.metric='minkowski'
-            self.n_jobs=4
+def __init__(self):
+    self.MethodsDatapkl='/Data/MethodsData.pkl'
+    self.MethodsMetricsDir='/Data/MethodsMetrics'
+    self.nneighbors=10
+    self.algorithm='auto'
+    self.metric='minkowski'
+    self.n_jobs=4
