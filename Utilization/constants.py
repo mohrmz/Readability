05c3fedf3a4b7d8fdc36a26495dbd364e4cc4ac2
mohrzm <mohammad0ramezani@gmail.com>
2022-01-18 00:55:54 +0330

    get_rootpath func add to contatnts madule

--- Utilization/constants.py
+++ Utilization/constants.py
@@ -8,12 +8,17 @@ algorithm='auto'
 metric='minkowski'
 n_jobs=4
 
-root_path = os.getcwd()
-root_path = root_path +'\\'
-root_path = root_path.replace('\\','/')
+
 
 PYTHONPATH = os.environ.get("PYTHONPATH") 
 
 
 undrestand_directory = PYTHONPATH.replace('\\','/')
 undrestand_directory = undrestand_directory.replace('Python','')
+
+
+def get_rootpath():
+    root_path = os.getcwd()
+    root_path = root_path +'\\'
+    root_path = root_path.replace('\\','/')
+    return root_path
--- Utilization/undrestand_facilities.py
+++ Utilization/undrestand_facilities.py
@@ -2,8 +2,9 @@ import os
 from .constants import *
 
 def create_understand_database_from_project():
+
     cmd = 'und create -db {0}{1}.udb -languages java add {2} analyze -all'
-    projects = [name for name in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, name)) if not os.path.isfile(os.path.join(root_path, name+".udb"))]
+    projects = [name for name in os.listdir(get_rootpath()) if os.path.isdir(os.path.join(get_rootpath(), name)) if not os.path.isfile(os.path.join(get_rootpath(), name+".udb"))]
     for project in projects:
-        command_ = cmd.format(root_path, project, root_path + project)
+        command_ = cmd.format(get_rootpath(), project, get_rootpath() + project)
         os.system('cmd /c "{0}"'.format(command_))
