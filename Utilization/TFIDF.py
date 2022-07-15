7a6495f312474a58bd34530b632b7792d44e7d51
mohmrz <mohammad0ramezani@gmail.com>
2022-07-15 10:58:21 +0430

    change tf idf functionality

--- Utilization/TFIDF.py
+++ Utilization/TFIDF.py
@@ -16,18 +16,19 @@ def calculate_TFIDF():
     methodpath = sourcemeter_tfidf_extract_pkl(Types.Method.name)
     classpath = sourcemeter_tfidf_extract_pkl(Types.Class.name)
     if not os.path.isfile(methodpath):
-        tf_if_pre_proccess_metric_csv_files(source_meter_pre_proccess_metric_csv_files(Types.Method))
+        tf_if_pre_proccess_metric_csv_files(source_meter_pre_proccess_metric_csv_files(Types.Method),Types.Method)
     if not os.path.isfile(classpath):
-        tf_if_pre_proccess_metric_csv_files(source_meter_pre_proccess_metric_csv_files(Types.Class))
+        tf_if_pre_proccess_metric_csv_files(source_meter_pre_proccess_metric_csv_files(Types.Class),Types.Class)
 
-def tf_if_pre_proccess_metric_csv_files(files):
-        sentences = files.iloc[1:20,1:2].values
+def tf_if_pre_proccess_metric_csv_files(files,type):
+        sentences = files.iloc[:,1:2].values
         #cleaning text
         stemmer = PorterStemmer()
         corpus = []
 
         for sent in sentences:
-            review = correct_names(sent[0])
+            print(sent[0])
+            review = correct_names(sent[0],type)
             review = re.sub("[^a-zA-Z]", " ", review)
             review = re.sub("\b[a-zA-Z]\b", " ", review)
             review = review.lower()
@@ -45,6 +46,7 @@ def tf_if_pre_proccess_metric_csv_files(files):
             df.sort_values(by=["tf_idf"],ascending=False)
             print(df)
             print('here')
+            df.to_csv(os.path.join(get_rootpath(),type.name+'tfidf-result.csv').replace('\\','/'), encoding='utf-8' ) 
 
 if __name__ == '__main__':   
     calculate_TFIDF()
